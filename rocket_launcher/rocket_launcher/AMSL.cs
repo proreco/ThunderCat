8b342cd967dbf3651d4218fb08ab72237deecf7f
usakgb <usakgb@gmail.cm>
2013-03-13 22:52:06 -0700

    initial asml-project added

---
+++ rocket_launcher/rocket_launcher/AMSL.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using UsbLibrary;
+using Microsoft.Win32;
+using System.IO;
+using System.Media;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace WindowsFormsApplication1
+{
+   public partial class AMSL : Form
+    {
+       Launcher control = new Launcher();
+
+        public AMSL()
+        {
+            InitializeComponent();
+        }
+        
+
+        //===============================LEFT===============================
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            
+            this.control.moveMissileLauncher(this.control.LEFT, 100);
+
+        }
+
+        //===============================RIGHT===============================
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer2.Enabled = true;
+            timer2.Start();
+
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer2.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            this.control.moveMissileLauncher(this.control.RIGHT, 100);
+
+        }
+
+        //===============================UP===============================
+
+        private void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer3.Enabled = true;
+            timer3.Start();
+
+        }
+
+        private void button3_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer3.Stop();
+        }
+
+
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+
+            this.control.moveMissileLauncher(this.control.UP, 100);
+
+        }
+
+        //===============================DOWN===============================
+
+        private void button4_MouseDown(object sender, MouseEventArgs e)
+        {
+            timer4.Enabled = true;
+            timer4.Start();
+
+        }
+
+        private void button4_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer4.Stop();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+
+            this.control.moveMissileLauncher(this.control.DOWN, 100);
+
+        }
+
+        //===============================FIRE===============================
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.control.moveMissileLauncher(this.control.FIRE, 5000); 
+        }
+
+        //==============================START===============================
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        //    int n = 1;
+        //    timer5.Enabled = true;
+            
+        //        timer5.Start();
+        //        do
+        //        {
+        //            this.control.moveMissileLauncher(this.control.LEFT, 1000);
+        //            this.control.moveMissileLauncher(this.control.RIGHT, 1000);
+
+        //        } while (n <= 1);
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            this.control.moveMissileLauncher(this.control.LEFT, 1000);
+        }
+
+        //===============================STOP===============================
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            timer5.Stop();
+    
+            this.control.SendUSBData(this.control.LED_OFF);
+        }
+        
+    }
+}
---
+++ rocket_launcher/rocket_launcher/Launcher.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using UsbLibrary;
+using Microsoft.Win32;
+using System.IO;
+using System.Media;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    class Launcher
+    {
+        List<string> _items = new List<string>();
+        private bool DevicePresent;
+
+        //Bytes used in command
+        public byte[] UP;
+        public byte[] RIGHT;
+        public byte[] LEFT;
+        public byte[] DOWN;
+
+        public byte[] FIRE;
+        public byte[] STOP;
+        public byte[] LED_OFF;
+        public byte[] LED_ON;
+
+        private UsbHidPort USB;
+
+        public Launcher()
+        {
+            //InitializeComponent();
+
+            _items.Add("One");
+            //TargetList.DataSource = _items;
+
+            this.UP = new byte[10];
+            this.UP[1] = 2;
+            this.UP[2] = 2;
+
+            this.DOWN = new byte[10];
+            this.DOWN[1] = 2;
+            this.DOWN[2] = 1;
+
+            this.LEFT = new byte[10];
+            this.LEFT[1] = 2;
+            this.LEFT[2] = 4;
+
+            this.RIGHT = new byte[10];
+            this.RIGHT[1] = 2;
+            this.RIGHT[2] = 8;
+
+            this.FIRE = new byte[10];
+            this.FIRE[1] = 2;
+            this.FIRE[2] = 0x10;
+
+            this.STOP = new byte[10];
+            this.STOP[1] = 2;
+            this.STOP[2] = 0x20;
+
+            this.LED_ON = new byte[9];
+            this.LED_ON[1] = 3;
+            this.LED_ON[2] = 1;
+
+            this.LED_OFF = new byte[9];
+            this.LED_OFF[1] = 3;
+
+            this.USB = new UsbHidPort();
+            this.USB.ProductId = 0;
+            this.USB.SpecifiedDevice = null;
+            this.USB.VendorId = 0;
+            this.USB.OnSpecifiedDeviceRemoved += new EventHandler(this.USB_OnSpecifiedDeviceRemoved);
+            this.USB.OnDataRecieved += new DataRecievedEventHandler(this.USB_OnDataRecieved);
+            this.USB.OnSpecifiedDeviceArrived += new EventHandler(this.USB_OnSpecifiedDeviceArrived);
+
+            this.USB.VID_List[0] = 0xa81;
+            this.USB.PID_List[0] = 0x701;
+            this.USB.VID_List[1] = 0x2123;
+            this.USB.PID_List[1] = 0x1010;
+            this.USB.ID_List_Cnt = 2;
+
+            IntPtr handle = new IntPtr();
+            this.USB.RegisterHandle(handle);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void USB_OnDataRecieved(object sender, DataRecievedEventArgs args)
+        {
+
+        }
+
+        private void USB_OnSpecifiedDeviceArrived(object sender, EventArgs e)
+        {
+            this.DevicePresent = true;
+            if (this.USB.ProductId == 0x1010)
+            {
+                this.command_switchLED(true);
+            }
+        }
+
+        private void USB_OnSpecifiedDeviceRemoved(object sender, EventArgs e)
+        {
+            this.DevicePresent = false;
+        }
+
+        public void command_switchLED(Boolean turnOn)
+        {
+            if (DevicePresent)
+            {
+                if (turnOn)
+                {
+                    this.SendUSBData(this.LED_ON);
+                }
+                else
+                {
+                    this.SendUSBData(this.LED_OFF);
+                }
+                this.SendUSBData(this.STOP);
+            }
+        }
+
+        public void SendUSBData(byte[] Data)
+        {
+            if (this.USB.SpecifiedDevice != null)
+            {
+                this.USB.SpecifiedDevice.SendData(Data);
+            }
+        }
+
+        public void moveMissileLauncher(byte[] Data, int interval)
+        {
+            if (DevicePresent)
+            {
+                this.command_switchLED(true);
+                SendUSBData(Data);
+                Thread.Sleep(interval);
+                this.SendUSBData(this.STOP);
+
+            }
+        }
+    }
+}
